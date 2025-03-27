71890cde47f7c8ec0e34c50fd07f3127f7ee3bd4
Deiiivy <deivypr28@gmail.com>
2025-03-27 18:59:51 -0500

    Terminada la clase Reactangulo

--- DibujarRectangulosFactory/Rectangulo.cs
+++ DibujarRectangulosFactory/Rectangulo.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 
 namespace DibujarRectangulosFactory
 {
-    class Rectangulo
+    public class Rectangulo : Figura
     {
+        public static int Contador { get; private set; } = 0;
+
+        public Rectangulo(int x, int y, Color color) : base(x, y, color)
+        {
+            Contador++;
+        }
     }
 }
