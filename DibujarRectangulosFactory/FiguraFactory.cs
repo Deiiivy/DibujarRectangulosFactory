f682be9c9464c0718b5555557f50f83fa9fa36a1
Deiiivy <deivypr28@gmail.com>
2025-03-27 19:01:07 -0500

    Terminada la clase FiguraFactory

--- DibujarRectangulosFactory/FiguraFactory.cs
+++ DibujarRectangulosFactory/FiguraFactory.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace DibujarRectangulosFactory
 {
-    class FiguraFactory
+    public static class FiguraFactory
     {
+        public static Figura CrearFigura(int x, int y, Color color)
+        {
+            return new Rectangulo(x, y, color);
+        }
     }
 }
